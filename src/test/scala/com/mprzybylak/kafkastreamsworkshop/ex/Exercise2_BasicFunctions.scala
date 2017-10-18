9c937e0bdbca4674e7cbaa34991dc1e8cd943b7d
Michał Przybylak <michal.przybylak@gmail.com>
2017-10-19 00:08:46 +0200

    expanding exercises

--- src/test/scala/com/mprzybylak/kafkastreamsworkshop/ex/Exercise2_BasicFunctions.scala
+++ src/test/scala/com/mprzybylak/kafkastreamsworkshop/ex/Exercise2_BasicFunctions.scala
@@ -12,10 +12,13 @@ class Exercise2_BasicFunctions extends KafkaStreamsTest {
 
   it should "capitalize first letter of words from input topic and pass it to output topic" in {
 
+    // GIVEN
     val inputTopic = Seq(("1", "lorem"), ("2", "ipsum"), ("3", "dolor"), ("4", "sit"), ("5", "amet"))
     val outputTopic = Seq(("1", "Lorem"), ("2", "Ipsum"), ("3", "Dolor"), ("4", "Sit"), ("5", "Amet"))
 
     MockedStreams()
+
+      // WHEN
       .topology(
         builder => {
           // `capitalize` method inputTopic class String allows to capitalize first letter
@@ -26,24 +29,65 @@ class Exercise2_BasicFunctions extends KafkaStreamsTest {
       )
       .config(config(strings, strings))
       .input(INPUT_TOPIC_NAME, strings, strings, inputTopic)
+
+      // THEN
       .output(OUTPUT_TOPIC_NAME, strings, strings, outputTopic.size) shouldEqual outputTopic
   }
 
   it should "filter out odd numbers from input topic" in {
+
+    // GIVEN
     val inputTopic = Seq[(String, Integer)](("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5))
     val outputTopic = Seq(("2", 2), ("4", 4))
 
     MockedStreams()
+
+      // WHEN
       .topology(
         builder => {
           val source: KStream[String, Integer] = builder.stream(INPUT_TOPIC_NAME)
-          val map: KStream[String, Integer] = source.filter((k, v) => v % 2 == 0)
-          map.to(OUTPUT_TOPIC_NAME)
+          val filter: KStream[String, Integer] = source.filter((k, v) => v % 2 == 0)
+          filter.to(OUTPUT_TOPIC_NAME)
         }
       )
       .config(config(strings, integers))
       .input(INPUT_TOPIC_NAME, strings, integers, inputTopic)
+
+      // THEN
       .output(OUTPUT_TOPIC_NAME, strings, strings, outputTopic.size) shouldEqual outputTopic
   }
+
+  "blog engine" should "log each request from request topic" in {
+
+    // GIVEN
+    val inputTopic: Seq[(Integer, String)] = Seq(
+      (12, "GET /posts"), // key = userId, value = request
+      (88, "GET /posts/72"),
+      (72, "POST /posts/72/comment { user: abc, comment: good article"),
+      (34, "POST /posts/72/comment { user: xyz, comment: I disagree"),
+      (72, "DELETE /posts/72/comment/15")
+    )
+    // TODO ADD LOGGER
+
+    val expectedLogs:Seq[String] = Seq[String](
+      "Request from user 12: GET /posts",
+      "Request from user 88: GET /posts/72",
+      "Request from user 72: POST /posts/72/comment { user: abc, comment: good article",
+      "Request from user 34: POST /posts/72/comment { user: xyz, comment: I disagree",
+      "Request from user 72: DELETE /posts/72/comment/15",
+    )
+
+    MockedStreams()
+
+      // WHEN
+      .topology(builder => {})
+      .config(config(integers, strings))
+      .input(INPUT_TOPIC_NAME, integers, strings, inputTopic)
+
+
+    // TODO assert logs
+  }
+
+  // TODO flatmapvalues
 
 }
